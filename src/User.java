f23ab08fbe5e9e6e54c9e27b1d617d52ee4ce765
spujol <spujol@inspladelestany.cat>
2025-03-20 16:35:32 +0100

    20/3/2025

--- src/User.java
+++ src/User.java
@@ -1,5 +1,3 @@
-import jdk.internal.org.jline.reader.SyntaxError;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,7 +5,7 @@ public class User {
     private String userID;
     private String userName;
     private String userType;
-    private Scanner sc = new Scanner(System.in);
+    private static Scanner sc = new Scanner(System.in);
 
     public User() {
         this.userID = "";
@@ -23,58 +21,57 @@ public class User {
     public java.lang.String getUserID() {
         return userID;
     }
-
     public void setUserID(java.lang.String userID) {
-        if (userType = "Customer") {
+        if (userType == "Customer") {
             System.err.println("You don't have permission to change the ID.");
         } else this.userID = userID;
     }
-
     public java.lang.String getUserName() {
         return userName;
     }
-
     public void setUserName(java.lang.String userName) {
-        if (userType = "Customer") {
+        if (userType == "Customer") {
             System.err.println("You don't have permission to change the username.");
         } else this.userName = userName;
     }
-
     public java.lang.String getUserType() {
         return userType;
     }
-
     public void setUserType(java.lang.String userType) {
-        if (userType = "Customer") {
+        if (userType == "Customer") {
             System.err.println("You don't have permission to change the user type.");
         } else this.userType = userType;
     }
-
     public static void addUser(ArrayList<User> userList) {
         System.out.println("What name do you want to give the user?");
         String name = sc.nextLine();
-
-        userList.add(new User(userList.size() + 1, name))
+        userList.add(new User(String.valueOf((userList.size() + 1)), name));
     }
 
     @Override
     public String toString() {
-        return  "UserID: " + userID +  +
+        return  "UserID: " + userID +
                 "\nUsername= " + userName +
                 "\nUsertype= " + userType +
                 '}';
     }
 
-    public void updateUser(){
-        if (userType = "Customer"){
+    public void updateUser(ArrayList<User> userList){
+        if (userType == "Customer"){
             System.out.println("You don't have permission to update users.");
         } else {
             System.out.println("Write the ID of the user that you want to change: ");
-            String oldID = sc.toString();
+            String oldID = sc.nextLine();
 
+            for (int i = 0; i < userList.size(); i++) {
+                if (userList.get(i).userID.equals(oldID)){
+
+                }
+            }
 
 
 
         }
+
     }
 }
