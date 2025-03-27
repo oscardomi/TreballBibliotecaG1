0b0ad5e2903a4204fdd112c907eabba1f2820ba0
spujol <spujol@inspladelestany.cat>
2025-03-27 15:03:48 +0100

    27/3/2025

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        User.addUser();
-        System.out.println(User.userList.get(0).getUserName());
-        User.userList.get(0).removeUser();
-        System.out.println(User.userList.get(0).getUserName());
-
-        User u1 = new User("2", "Josefina", "Administrator");
-        System.out.println(u1.getUserName());
-        u1.removeUser();
-        System.out.println(u1.getUserName());
 
     }
 }
